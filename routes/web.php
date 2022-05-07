2d5b095493811c235dd69d4b5be42ca4b2dff6f0
Kou <hikkappi@yahoo.co.jp>
2022-05-07 13:55:49 +0900

    add: sanctum認証のデモコードを実装。

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
+declare(strict_types=1);
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/login', LoginController::class)->name('login');
+Route::post('/logout', LogoutController::class)->name('logout');
